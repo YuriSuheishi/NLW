f71986b607cfc6104d26a128451d17cf660760f6
YuriSuheishi <yuri.suheishi@hotmail.com>
2021-02-23 23:14:43 -0300

    Dia 2 Rota NodeJs

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,21 +1,11 @@
+import 'reflect-metadata'
 import express from 'express';
+import "./database";
+import { router } from './routes';
 
 const app = express();
-/**
- * GET => BUSCA
- * POST => SALVA
- * PUT => ALTERA
- * DELETE => DELETA
- * PATCH => ALTERAÇÃO ESPECIFICA   
- */
 
- // https://localhost:3333/
- app.get("/", (request, response) => {
-     return response.json("Hello!")
- });
-
- app.post("/", (request, response) => {
-     return response.json({message :"Salvo com sucesso!"})
- });
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("server is running!"));
